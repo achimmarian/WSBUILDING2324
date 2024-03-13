d06d680d3905fd82c4f54fb982c805f61bb91fa8
achimmarian <150622961+achimmarian@users.noreply.github.com>
2024-03-13 21:11:00 +0200

    curs1

---
+++ cvtemplate/src/index.js
@@ -0,0 +1,16 @@
+// Import our custom CSS
+import './scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+import particles from 'particles.js'
+import './js/particles.js'
+
+// Import https://github.com/mattboldt/typed.js/
+// import Typed from 'typed.js';
+import '../node_modules/typed.js/dist/typed.umd.js'
+
+import './js/main.js';
+
+import '../node_modules/typed.js/dist/typed.umd.js'
